1b4c427b8aa6b75ba2477a2b9ae9bf51c40fe22d
Rocco Silvestrini <roccosilvestrini@hotmail.com>
2026-07-08 11:42:07 -0300

    Configuración inicial de Next.js y deploy a DonWeb

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Configuración necesaria para exportación estática
+  output: 'export',
+  assetPrefix: './',
+
   images: {
+    // Necesario para exportación estática (no puedes usar el optimizador de imágenes de Next.js)
+    unoptimized: true,
+    
+    // Tus configuraciones de seguridad
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
